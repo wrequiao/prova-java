183fe5aef2151238fb4617a2705312306e5956ad
wrequiao <wrequiao@gmail.com>
2020-10-15 14:07:45 -0300

    Ajuste

--- src/main/java/br/com/confidencecambio/javabasico/Validacoes.java
+++ src/main/java/br/com/confidencecambio/javabasico/Validacoes.java
@@ -10,14 +10,15 @@ import javax.validation.ValidatorFactory;
 
 import br.com.confidencecambio.entidade.Cliente;
 import br.com.confidencecambio.entidade.EntidadeBase;
+import br.com.confidencecambio.entidade.Gerente;
+import br.com.confidencecambio.entidade.Robo;
 
 public class Validacoes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		EntidadeBase entidade = new EntidadeBase();
-		//entidade.setNome(" Maria Madalena Batista Silva");
-		entidade.setNome("");
+		entidade.setNome(" Maria Madalena Batista Silva");
 		 System.out.println (entidade.primeiroNome());
 		 System.out.println (entidade.maisuculo());
 		 System.out.println (entidade.ultimoNome());
@@ -35,7 +36,7 @@ public class Validacoes {
 	      System.out.println (msgError);
 		}
 		
-		/*
+		
 		Cliente cliente = new Cliente("12345", "Maria de Deus");
 		 System.out.println (cliente.primeiroNome());
 		 System.out.println (cliente.maisuculo());
@@ -53,7 +54,45 @@ public class Validacoes {
 	      String msgError = error.getMessage();
 	      System.out.println (msgError);
 		}
-		*/
+		
+		
+		Gerente gerente = new Gerente("34566", "Antonio Carlos da Silva");
+		 System.out.println (gerente.primeiroNome());
+		 System.out.println (gerente.maisuculo());
+		 System.out.println (gerente.ultimoNome());
+		 System.out.println (gerente.nomeAbreviado());
+		
+		
+		factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator ();
+
+		Set<ConstraintViolation<Gerente>> constraintViolationsGerente = validator.validate(gerente);
+
+		for (ConstraintViolation error: constraintViolationsGerente) 
+		{
+	      String msgError = error.getMessage();
+	      System.out.println (msgError);
+		}
+		
+		
+		Robo robo = new Robo("12345", "R2D2 XPTO");
+		 System.out.println (robo.primeiroNome());
+		 System.out.println (robo.maisuculo());
+		 System.out.println (robo.ultimoNome());
+		 System.out.println (robo.nomeAbreviado());
+		
+		
+		factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator ();
+
+		Set<ConstraintViolation<Robo>> constraintViolationsRobo = validator.validate(robo);
+
+		for (ConstraintViolation error: constraintViolationsRobo) 
+		{
+	      String msgError = error.getMessage();
+	      System.out.println (msgError);
+		}
+		
 	}
 
 }
